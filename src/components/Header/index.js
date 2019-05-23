ccaf8dbd1d6d9b259b454df1e3e763efa6ffe439
Jeison Pedroso <jeison.contas@gmail.com>
2019-05-23 18:05:48 -0300

    commit inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+
+// class Header extends Component{
+//     render(){
+//         return <h1>Hello Word</h1>
+//     }
+// }
+
+const Header = ()=>(
+    <header id="main-header">React JS</header>
+);
+
+export default Header;
